02e2173e0f25ecddbc082359fcb9faa0c83be2a4
Alexander Morozov <mail@alex-morozov.com>
2025-03-22 21:30:59 +0300

    feat: prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,24 @@
-import {Button} from "@/shared/ui/button";
+import { Button } from "@/shared/ui/button";
+import { prisma } from "@/shared/lib/db";
+import { Card, CardTitle } from "@/shared/ui/card";
 
-export default function Home() {
+export default async function Home() {
+  const games = await prisma.game.findMany();
+  console.log(games);
+
   return (
-   <div>
-     <Button size='lg' variant='destructive'>Hello</Button>
-   </div>
+    <div>
+      <Button size="lg" variant="destructive">
+        Hello
+      </Button>
+
+      {games.map((game) => {
+        return (
+          <Card key={game.id}>
+            <CardTitle>{game.name}</CardTitle>
+          </Card>
+        );
+      })}
+    </div>
   );
 }
